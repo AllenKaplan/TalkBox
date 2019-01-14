c3f327fbd9c696927376858808dc59f6adfba602
Allen Kaplan <allenkap@my.yorku.ca>
2019-01-14 12:20:46 -0500

    Test

--- TalkBox/src/Main.java
+++ TalkBox/src/Main.java
@@ -2,13 +2,7 @@
 public class Main {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		System.out.println("Hello World");
-=======
-		System.out.println("Hello World");
-		System.out.println("Second commit");
-		System.err.println(" nice text");
->>>>>>> branch 'master' of https://github.com/AllenKaplan/TalkBox.git
 	}
 
 }
