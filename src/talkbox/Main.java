449e56124ac2bf784ba1cdc3ad3018fb4df9463b
Matt Walker <matt.walker98@outlook.com>
2019-01-21 21:09:03 -0500

    Added text-to-speech capability via MaryTTS

--- src/talkbox/Main.java
+++ src/talkbox/Main.java
@@ -1,5 +1,12 @@
 package talkbox;
 
+import java.io.IOException;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.scene.Scene;
@@ -9,21 +16,27 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
-
+import marytts.LocalMaryInterface;
+import marytts.MaryInterface;
+import marytts.exceptions.MaryConfigurationException;
+import marytts.exceptions.SynthesisException;
 
 public class Main extends Application  {
 
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-    
-		Sentence newPhrase = new Sentence();
     	
+    	MaryInterface marytts = new LocalMaryInterface();
+    	System.out.println(marytts.getAvailableOutputTypes());
+		Sentence newPhrase = new Sentence();
+		
         primaryStage.setTitle("TalkBox Experiment 1");
         primaryStage.setHeight(400);
         primaryStage.setWidth(1000);
 
-        Label label1 = new Label("");
+        Label label1 = new Label(" ");
+        label1.setText("\"" + newPhrase.getConstructedScentence() + "\"");
         
         ComboBox<String> cb1 = new ComboBox<String>(FXCollections.observableArrayList(
         		"me", "you", "they")
@@ -31,7 +44,7 @@ public class Main extends Application  {
         cb1.setPromptText("Set Subject");
         cb1.setOnAction( value -> {
         	newPhrase.addWord(new Word(Part_Of_Speech.Subject, Phrase_Type.Noun, cb1.getValue()));
-        	label1.setText("Scentence: \"" + newPhrase.getConstructedScentence() + "\"");
+        	label1.setText("\"" + newPhrase.getConstructedScentence() + "\"");
         });
         
         ComboBox<String> cb2 = new ComboBox<String>(FXCollections.observableArrayList(
@@ -40,7 +53,7 @@ public class Main extends Application  {
         cb2.setPromptText("Set Subject");
         cb2.setOnAction( value -> {
         	newPhrase.addWord(new Word(Part_Of_Speech.Verb, Phrase_Type.Verb, cb2.getValue()));
-        	label1.setText("Scentence: \"" + newPhrase.getConstructedScentence() + "\"");
+        	label1.setText("\"" + newPhrase.getConstructedScentence() + "\"");
         });
         
         ComboBox<String> cb3 = new ComboBox<String>(FXCollections.observableArrayList(
@@ -49,22 +62,42 @@ public class Main extends Application  {
         cb3.setPromptText("Set Object");
         cb3.setOnAction(value -> {
         	newPhrase.addWord(new Word(Part_Of_Speech.Object, Phrase_Type.Noun, cb3.getValue()));
-        	label1.setText("Scentence: \"" + newPhrase.getConstructedScentence() + "\"");
+        	label1.setText("\"" + newPhrase.getConstructedScentence() + "\"");
         });
         
         ToggleButton button6 = new ToggleButton("Toggle Type of Question");
         button6.setOnAction(value -> {
         	newPhrase.setQuestion(button6.isSelected());
-        	label1.setText("Scentence: \"" + newPhrase.getConstructedScentence() + "\"");
+        	label1.setText("\"" + newPhrase.getConstructedScentence() + "\"");
         });
         
         Button button7 = new Button("Change Tense");
         button7.setOnAction( value -> {
         	newPhrase.rotateTense();
-        	label1.setText("Scentence: \"" + newPhrase.getConstructedScentence() + "\"");
+        	label1.setText("\"" + newPhrase.getConstructedScentence() + "\"");
         });
         
-        HBox hbox = new HBox(cb1, cb2, cb3, button6, button7, label1);
+        Button button8 = new Button("PLAY");
+        button8.setOnAction( value -> {
+    		AudioInputStream audio;
+			try {
+				Clip clip = AudioSystem.getClip();
+				audio = marytts.generateAudio(label1.getText());
+	    		clip.open(audio);
+	    		clip.start();
+			} catch (SynthesisException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (LineUnavailableException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+        });
+        
+        HBox hbox = new HBox(cb1, cb2, cb3, button6, button7, button8, label1);
 
         Scene scene = new Scene(hbox, 400, 100);
         primaryStage.setScene(scene);
